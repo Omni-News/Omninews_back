da0943e25a142ad549f3f2ff7da747f55e8a3a74
KangDongHyeon <kang1027@KangDongHyeons-MacBook-Air.local>
2025-10-27 09:09:07 +0900

    fix: adapt pagenation with get channel items

--- src/dto/rss/request.rs
+++ src/dto/rss/request.rs
@@ -2,6 +2,14 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
+pub struct RssItemRequestDto {
+    #[schemars(example = "example_channel_id")]
+    pub channel_id: Option<i32>,
+    #[schemars(example = "example_page")]
+    pub page: Option<i32>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
 pub struct CreateRssRequestDto {
     #[schemars(example = "example_rss_link")]
     pub rss_link: String,
@@ -24,5 +32,13 @@ fn example_rss_id() -> i32 {
 }
 
 fn example_num() -> i32 {
+    1
+}
+
+fn example_channel_id() -> i32 {
+    5
+}
+
+fn example_page() -> i32 {
     1
 }
